ab04122adf629b3a4cb42928fb019cebb0a351f0
Fede Cugliandolo <fede.cugliandolo@gmail.com>
2017-07-19 23:25:50 -0300

    add and remove several players -> /juega & /baja commands

--- Sources/App/Bot.swift
+++ Sources/App/Bot.swift
@@ -11,46 +11,61 @@ import Vapor
 public class Bot {
     var droplet : Droplet
     var token: String
-    var message : String
-    var user : User
-    var chatID : String
-    var callBackQueryText : String
-    var callBackMessageID : String
-    var callBackChatID : String
-    var callBackQueryID : String
-
+    var message = ""
+    var user = User()
+    var chatID = ""
+    
+    // callback TODO: struct for callbacks?
+    var callBackQueryText = ""
+    var callBackMessageID = ""
+    var callBackChatID = ""
+    var callBackQueryID = ""
+    var callBackQueryData = ""
+    var callBackQueryDataType = CallbackType.Juego
+    
     public init(token myToken:String, droplet drop:Droplet) {
         droplet = drop
         token = myToken
-        
-        message = ""
-        chatID = ""
-        callBackQueryText = ""
-        callBackChatID = ""
-        callBackMessageID = ""
-        user = User()
-        callBackQueryID = ""
     }
     
-
+    
     public func processMessage(req request:Request) throws -> JSON {
         
         message = (request.data["message", "text"]?.string ?? "").lowercased()
         user = getUser(request)
         chatID = request.data["message", "chat", "id"]?.string ?? ""
+        
+        
         callBackQueryText = request.data["callback_query","message", "text"]?.string ?? ""
         callBackMessageID = request.data["callback_query","message","message_id"]?.string ?? ""
         callBackChatID = request.data["callback_query","message","chat","id"]?.string ?? ""
         callBackQueryID = request.data["callback_query","id"]?.string ?? ""
+        callBackQueryData = request.data["callback_query","data"]?.string ?? ""
+        callBackQueryDataType = callBackQueryData == "cancel" ? CallbackType.Cancel : callBackQueryData.lowercased().contains("baja") ? CallbackType.Baja : CallbackType.Juego
+
         
-        if !callBackQueryText.isEmpty {
-            let p =  getCallBackUser(request)
-            if players.add(player: p) {
+        if !callBackQueryText.isEmpty { // callback Query
+            
+            switch callBackQueryDataType {
+            
+            case .Baja:
+                let userData = callBackQueryData.commaSeparatedArray()
+                let pID = players.list.filter { $0.id == userData[1] }
+                let pCompleteName = players.list.filter { $0.completeName() == userData[2] }
+                return try removePlayer(pID.count == 1 ? pID[0] : pCompleteName[0], pID.count > 1)
+                
+            case .Cancel:
                 return try showList(nil)
-            } else {
-                return try callbackAnswer(answer: "\(p.firstName), ya estás anotado 🤙🏻")
+
+            default:
+                let p =  getCallBackUser(request)
+                if players.add(player: p) {
+                    return try showList(nil)
+                } else {
+                    return try callbackAnswer(answer: "\(p.firstName), ya estás anotado 🤙🏻")
+                }
             }
-        
+            
         } else {
             
             switch message {
@@ -76,14 +91,10 @@ public class Bot {
                 
             case "/nuevalista":
                 return try newList()
-            
+                
             case "/mebajo":
                 let p = getUser(request)
-                if players.remove(player: p) {
-                    return try showList("‼️ Se bajó \(p.completeName())")
-                } else {
-                    return try showList("🤦🏻‍♂️ No estás anotado, \(p.firstName)\n¡Anotate! 👇🏻")
-                }
+                return try removePlayer(p , nil)
                 
             case _ where message.lowercased().contains("/canchade"):
                 if let max = Int(message.lowercased().replacingOccurrences(of: "/canchade", with: "").trim()) {
@@ -91,15 +102,26 @@ public class Bot {
                 }
                 return try self.showList("Hasta \(players.maxPlayers) titulares")
                 
-                case "/golazo":
-                    return try JSON(node: [
-                        "method": "sendDocument",
-                        "chat_id": chatID,
-                        "caption": "¿¿Pidieron a Iniesta??",
-                        "document": "CgADAQADDQADC3xwR9T4eWaEHnjMAg",
-                        "disable_notification": true,
-                        ])
-
+            case "/golazo":
+                return try JSON(node: [
+                    "method": "sendDocument",
+                    "chat_id": chatID,
+                    "caption": "¿¿Pidieron a Iniesta??",
+                    "document": "CgADAQADDQADC3xwR9T4eWaEHnjMAg",
+                    "disable_notification": true,
+                    ])
+                
+            case _ where message.lowercased().hasPrefix("/juega"):
+                let playersArray = message.lowercased().replacingOccurrences(of: "/juega", with: "").trim().commaSeparatedArray()
+                for p in playersArray {
+                    let newPlayer = User(id: "falopa", firstName: p, lastName: "(invitado)", alias: "p")
+                    players.addGuest(player: newPlayer)
+                }
+                return try showList("Gracias \(getUser(request).firstName) por agregar jugadores 🙌🏻")
+                
+            case "/baja":
+                return try showBajaKeyboard()
+            
             default:
                 return try JSON(node: [
                     "message": "Está vacío"])
@@ -108,15 +130,23 @@ public class Bot {
     }
     
     func newList() throws -> JSON {
-        let j1 = User(id: "1", firstName: "Jugador", lastName: "1", alias: "j1")
-        let j2 = User(id: "2", firstName: "Jugador", lastName: "2", alias: "j2")
-        let j3 = User(id: "3", firstName: "Jugador", lastName: "3", alias: "j3")
-        let j4 = User(id: "4", firstName: "Jugador", lastName: "4", alias: "j4")
-        let j5 = User(id: "5", firstName: "Jugador", lastName: "5", alias: "j5")
-        let j6 = User(id: "6", firstName: "Jugador", lastName: "6", alias: "j6")
-//        let j7 = User(id: "7", firstName: "Jugador", lastName: "7", alias: "j7")
-        players = Players(list: [j1, j2, j3, j4, j5, j6], maxPlayers: 6)
+//        let j1 = User(id: "1", firstName: "Jugador", lastName: "1", alias: "j1")
+//        let j2 = User(id: "2", firstName: "Jugador", lastName: "2", alias: "j2")
+//        let j3 = User(id: "3", firstName: "Jugador", lastName: "3", alias: "j3")
+//        let j4 = User(id: "4", firstName: "Jugador", lastName: "4", alias: "j4")
+//        let j5 = User(id: "5", firstName: "Jugador", lastName: "5", alias: "j5")
+//        let j6 = User(id: "6", firstName: "Jugador", lastName: "6", alias: "j6")
+        //        let j7 = User(id: "7", firstName: "Jugador", lastName: "7", alias: "j7")
+        players = Players(list: [], maxPlayers: 12)
         return try showList(nil)
+    }
+    
+    func removePlayer(_ player:User, _ byName: Bool?) throws -> JSON {
+        if players.remove(player: player, byName) {
+            return try showList("‼️ Se bajó \(player.completeName())")
+        } else {
+            return try showList("🤦🏻‍♂️ No estás anotado, \(player.firstName)\n¡Anotate! 👇🏻")
+        }
     }
     
     func getUser (_ request:Request) -> User {
@@ -135,32 +165,33 @@ public class Bot {
         return u
     }
     
-    var players = Players(list: [], maxPlayers: 6)
+    var players = Players(list: [], maxPlayers: 12)
     
     func showList(_ extra:String?) throws -> JSON {
         let title = players.list.count > 0 ? "Para este jueves somos *\(players.list.count)*" : "Anótensennn para jugar"
         let extraMessage = "\n\n*\(extra ?? "")*"
         let messageComplete = title + players.show() + extraMessage
         
-        if !message.isEmpty {
-        return try JSON(node: [
-            "method": "sendMessage",
-            "chat_id": chatID,
-            "text": messageComplete,
-            "parse_mode": "Markdown",
-            "reply_markup": try JSON(node: [
-                "inline_keyboard": try JSON (node: [
-                    try JSON (node: [
+        if !message.isEmpty { // mensaje comun
+            return try JSON(node: [
+                "method": "sendMessage",
+                "chat_id": chatID,
+                "text": messageComplete,
+                "parse_mode": "Markdown",
+                "reply_markup": try JSON(node: [
+                    "inline_keyboard": try JSON (node: [
                         try JSON (node: [
-                            "text": "⚽️ ¡Juego!",
-                            "callback_data": "juego"
-                            ]),
+                            try JSON (node: [
+                                "text": "⚽️ ¡Juego!",
+                                "callback_data": "juego"
+                                ]),
+                            ])
                         ])
                     ])
                 ])
-            ])
             
-        } else {
+        } else { //calback query
+            
             return try JSON(node: [
                 "method": "editMessageText",
                 "chat_id": self.callBackChatID,
@@ -178,7 +209,7 @@ public class Bot {
                         ])
                     ]),
                 ])
-        
+            
         }
     }
     
@@ -190,13 +221,53 @@ public class Bot {
             "show_alert": true
             ])
     }
+    
+    func showBajaKeyboard() throws -> JSON {
+        return try JSON(node: [
+            "method": "sendMessage",
+            "chat_id": chatID,
+            "text": "¿Quién se baja?",
+            "parse_mode": "Markdown",
+            "reply_markup": try JSON(node: [
+                "inline_keyboard": try getPlayersJSON()
+                ])
+            ])
+    }
+    
+    func makeBajaButton(player:User) throws -> [JSON] {
+        var json = JSON()
+        try json.set("text", player.completeName())
+        try json.set("callback_data", "baja,"+"\(player.id),"+"\(player.completeName())")
+        let array = [json]
+        return array
+    }
+    
+    func makeCancelButton(textoAlternativo:String?) throws -> [JSON] {
+        var json = JSON()
+        try json.set("text", textoAlternativo ?? "Cancelar")
+        try json.set("callback_data", "cancel")
+        let array = [json]
+        return array
+    }
+    
+    func getPlayersJSON () throws -> [[JSON]] {
+        var jsonArray = [[JSON()]]
+        for p in players.list {
+            jsonArray.append(try makeBajaButton(player: p))
+        }
+        jsonArray.remove(at: 0)
+        jsonArray.insert(try makeCancelButton(textoAlternativo: "LISTO"), at: 0)
+        return jsonArray
+    }
 }
 
 struct Players {
     
     var list : [User]
     var maxPlayers : Int
-
+    
+    mutating func addGuest(player: User) { list.append(player) }
+    
     mutating func add(player:User) -> Bool {
         if !list.contains(where: { $0.id == player.id }) {
             list.append(player)
@@ -204,9 +275,9 @@ struct Players {
         } else { return false }
     }
     
-    mutating func remove(player:User) -> Bool {
-        if list.contains(where: { $0.id == player.id }),
-            let idx = list.index(where: { $0.id == player.id }) {
+    mutating func remove(player:User, _ byName: Bool?) -> Bool {
+        if list.contains(where: { byName == true ? $0.completeName() == player.completeName() : $0.id == player.id }),
+            let idx = list.index(where: { byName == true ? $0.completeName() == player.completeName() : $0.id == player.id }) {
             list.remove(at: idx)
             return true
         } else { return false }
@@ -216,7 +287,7 @@ struct Players {
         var listMessage = "\n"
         for (index,player) in list.enumerated() {
             if (index + 1) == self.maxPlayers + 1 {
-                listMessage.append("\n\n_Comen banco:_ \n")
+                listMessage.append("\n\n_Pueden ir a filmar:_ \n")
             }
             listMessage.append("\n" + " \(index + 1). " + player.firstName + " " + player.lastName)
         }
@@ -231,4 +302,10 @@ struct User {
     var alias:String = ""
     
     public func completeName() -> String { return "\(firstName) " + " \(lastName)" }
+}
+
+enum CallbackType: String {
+    case Juego = "juego"
+    case Cancel = "cancel"
+    case Baja = "baja"
 }
